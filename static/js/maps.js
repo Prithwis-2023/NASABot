354c7dc2da39e5f12532f6e0d48a4a1e6ae78460
Prithwis Das <prithwisdas2017@gmail.com>
2023-10-16 07:54:07 -0700

    update

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,4 +1,5 @@
 const x = document.getElementById("demo");
+var req = new XMLHttpRequest();
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -9,6 +10,20 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
+  //x.innerHTML = "Latitude: " + position.coords.latitude +
+  //"<br>Longitude: " + position.coords.longitude;
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+  url = 'https://atlas.microsoft.com/weather/currentConditions/json?api-version=1.0&query=' + position.coords.latitude + '%2C' + position.coords.longitude + '&subscription-key=Ks60X1ByPJCoOo7bjON2hZnwm0N1mhBOqYvkia5BVcQ';
+  console.log(url);
+  req.open("GET", url);
+  req.send();
+
+  req.addEventListener("load", function(){
+    if(req.status == 200 && req.readyState == 4){
+        var response = JSON.parse(req.responseText);
+        //now extraction!
+    }
+  });      
 }
+
