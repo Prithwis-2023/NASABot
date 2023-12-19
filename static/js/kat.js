79de5888d4499df1c0c1536fc4662c7894aeddfb
Prithwis Das <prithwisdas2017@gmail.com>
2023-12-19 11:07:51 -0800

    update

--- static/js/kat.js
+++ static/js/kat.js
@@ -3,8 +3,9 @@ console.log('import success');
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("kat-submit").addEventListener("click", function() {
-        var prompt;
-        retrieveText();
+        // Get the value from the textbox
+        var prompt = document.getElementById("exampleFormControlTextarea1").value;
+
         const API_KEY = 'AIzaSyDhURas96RlzIs84VD_r9b6NWeV_fR0SHo';
         const genAI = new GoogleGenerativeAI(API_KEY);
         async function run() {
@@ -23,15 +24,6 @@ document.addEventListener('DOMContentLoaded', function() {
         run();
     });
     function retrieveText() {
-        // Get the value from the textbox
-        var userInput = document.getElementById("exampleFormControlTextarea1").value;
 
-        // Do something with the retrieved text
-        console.log("Text retrieved: " + userInput);
-
-        // You can store the value in a variable or perform any other actions here
-        var storedText = userInput;
-        console.log("Stored text: " + storedText);
-        prompt = storedText;
     }
 });    
